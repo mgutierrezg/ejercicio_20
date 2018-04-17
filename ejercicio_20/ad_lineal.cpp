95235ec9a15550f811f6bf5892729b61fd8c684c
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-04-17 07:20:52 -0500

    Add files via upload

---
+++ ejercicio_20/ad_lineal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+double* func_inicial(double p1, double p2, soble p3, double p4)
+double* func_inicial(double p1, double p2, soble p3, double p4){
+  double x = [4.0];
+}
