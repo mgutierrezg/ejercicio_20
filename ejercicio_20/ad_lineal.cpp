453fccfc35536894cdc55bb6d21edd19f13d5765
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-04-17 12:21:11 +0000

    Delete ad_lineal.cpp

--- ejercicio_20/ad_lineal.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-double* func_inicial(double p1, double p2, soble p3, double p4)
-double* func_inicial(double p1, double p2, soble p3, double p4){
-  double x = [4.0];
-}
