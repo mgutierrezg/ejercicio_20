d8d7dd2a63fc7745ffa9fadfa7311632518a2d01
mgutierrezg <37335456+mgutierrezg@users.noreply.github.com>
2018-04-17 07:52:17 -0500

    ad lineal sin resolver eq

---
+++ ad_lineal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void u_in(double* u,double x_min,double delta_x, int n){
+
+  for (int i=0; i<n; i++){
+    double x = x_min + i;
+    if(x>0.75 && x<=1.25){
+      u[i] = 1.0;
+    }
+    else{
+      u[i] = 0;
+    }
+  }
+}
+
+void columnas(double* u,double x_min,double delta_x, int n){
+  for (int i=0; i<n; i++){
+    double x = x_min + i;
+    u_in(u,x_min, delta_x,n);
+    cout<< x << "  "<< u[i] <<endl;
+  }
+}
+int main(){
+  double *u;
+  double delta_x = 0.1, delta_t = 0.01;
+  double x_min = 0.0, x_max = 2.0;
+  double T = 1.0, c =1.0;
+
+  int n = (x_max-x_min)/delta_x + 1;
+  u = new double[n];
+  u_in(u,x_min,delta_x, n);
+  columnas(u,x_min,delta_x, n);
+  return 0;
+}
